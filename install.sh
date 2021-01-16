5d68f900ce820a33af304b1eb2ae1f4835a8feb1
VanL <van.lindberg@gmail.com>
2021-01-16 11:41:58 -0600

    Fix typo.

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 service="pods-compose.service"
 
 cp -v systemd/${service} /etc/systemd/system/
-mkdir -p /etc/pdds-compose
+mkdir -p /etc/pods-compose
 cp -v pods-compose.ini /etc/pods-compose
 echo "Default pods-compose.ini placed in /etc/pods-compose."
 echo "Per-user settings can be placed in $HOME/.pods-compose.ini 
