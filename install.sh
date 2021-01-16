0d635825e30f7e0e103f090ef1c7d98b4be2a2dd
VanL <van.lindberg@gmail.com>
2021-01-16 11:32:04 -0600

    Update install.sh to place pods-compose.ini in /etc
    Place pods-compose.ini in /etc/pods-compose, and explain that per-user settings are available.

--- install.sh
+++ install.sh
@@ -3,5 +3,8 @@
 service="pods-compose.service"
 
 cp -v systemd/${service} /etc/systemd/system/
-cp -v pods-compose.ini /usr/local/bin/
+mkdir -p /etc/pdds-compose
+cp -v pods-compose.ini /etc/pods-compose
+echo "Default pods-compose.ini placed in /etc/pods-compose."
+echo "Per-user settings can be placed in $HOME/.pods-compose.ini 
 cp -v pods-compose.py /usr/local/bin/pods-compose
